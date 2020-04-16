a05f873a721a81a7af5949c3cac838e21dcabd10
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-04-17 02:23:12 +0300

    Exact Sum of Real Numbers

---
+++ Lab3.java
@@ -0,0 +1,27 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Lab3
+{
+
+	public static void main(String[] args)
+	{
+		
+			Scanner sc = new Scanner(System.in);
+			
+			int n = Integer.parseInt(sc.nextLine());
+
+			BigDecimal sum = new BigDecimal(0);
+
+			for (int i = 0; i < n; i++) {
+
+				BigDecimal number = new BigDecimal(sc.nextLine());
+
+				sum = sum.add(number);
+
+			}
+
+			System.out.println(sum);
+	}
+
+}
