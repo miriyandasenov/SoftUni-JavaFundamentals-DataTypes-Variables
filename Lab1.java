6df2260d4a63de64ee3b59bf65bdfe1fd3195634
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-04-16 04:01:20 +0300

    Convert meters to kilometres

---
+++ Lab1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lab1
+{
+
+	public static void main(String[] args)
+	{
+		
+			Scanner sc = new Scanner(System.in);
+			
+			int meters = Integer.parseInt(sc.nextLine());
+			double kilometers = meters / 1000.0;
+			
+			System.out.printf("%.2f", kilometers);
+	}
+
+}
